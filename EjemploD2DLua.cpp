b020d04fd9c42d77bebd9b5560623c30cf234d89
Lab301 <Lab301@gmail.com>
2025-03-07 13:20:31 -0600

    Agrega funciones de drawline, drawRect

--- EjemploD2DLua.cpp
+++ EjemploD2DLua.cpp
@@ -57,6 +57,10 @@ int Clear(lua_State* L)
 {
 	if (renderTarget != NULL)
 	{
+		float r = (float)lua_tonumber(L, 1) /255;
+		float g = (float)lua_tonumber(L, 2) /255;
+		float b = (float)lua_tonumber(L, 3) /255;
+		clearColor = D2D1::ColorF(r, g, b);
 		renderTarget->Clear(clearColor);
 	}
 	return 0;
@@ -66,10 +70,11 @@ int SetBrushColor(lua_State* L)
 {
 	if (renderTarget != NULL)
 	{
-		float r = (float)lua_tonumber(L, 1);
-		float g = (float)lua_tonumber(L, 2);
-		float b = (float)lua_tonumber(L, 3);
-		HRESULT hr = renderTarget->CreateSolidColorBrush(D2D1::ColorF(r, g, b), &brush);
+		int r = (int)lua_tonumber(L, 1);
+		int g = (int)lua_tonumber(L, 2);
+		int b = (int)lua_tonumber(L, 3);
+
+		HRESULT hr = renderTarget->CreateSolidColorBrush(D2D1::ColorF(r/(float)255, g/ (float)255, b/ (float)255), &brush);
 	}
 	return 0;
 }
@@ -85,12 +90,38 @@ int DrawCircle(lua_State* L)
 	return 0;
 }
 
+int DrawRect(lua_State* L)
+{
+	if (renderTarget != NULL)
+	{
+		float x = (float)lua_tonumber(L, 1);
+		float y = (float)lua_tonumber(L, 2);
+		float w = (float)lua_tonumber(L, 3);
+		float h = (float)lua_tonumber(L, 4);
+		renderTarget->FillRectangle(D2D1::RectF(x, y, x + w, y + h), brush);
+	}
+	return 0;
+}
+
+int DrawLine(lua_State* L)
+{
+	if (renderTarget != NULL)
+	{
+		float x1 = (float)lua_tonumber(L, 1);
+		float y1 = (float)lua_tonumber(L, 2);
+		float x2 = (float)lua_tonumber(L, 3);
+		float y2 = (float)lua_tonumber(L, 4);
+		renderTarget->DrawLine(D2D1::Point2F(x1, y1), D2D1::Point2F(x2, y2), brush);
+	}
+	return 0;
+}
+
 void luaDraw(lua_State* L, float dt)
 {
 	lua_getglobal(L, "Draw");
 	if (lua_isfunction(L, -1))
 	{
-		Log(L"Calling draw function from lua");
+		//Log(L"Calling draw function from lua");
 		lua_pushnumber(L, dt);
 		if (lua_pcall(L, 1, 0, 0) != 0)
 		{
@@ -108,9 +139,11 @@ int lua_mymodule(lua_State* L)
 {
 	static const luaL_Reg myModule[] =
 	{
-	{ "clear", Clear },
-	{ "setBrushColor", SetBrushColor },
-	{ "drawCircle", DrawCircle },
+	{ "Clear", Clear },
+	{ "SetBrushColor", SetBrushColor },
+	{ "DrawCircle", DrawCircle },
+	{ "DrawRect", DrawRect },
+	{ "DrawLine", DrawLine },
 	{ NULL, NULL }
 	};
 	luaL_newlib(L, myModule);
@@ -183,7 +216,7 @@ int main(int argc, char** argv)
 	ShowWindow(hWnd, SW_SHOW);
 
 	luaL_openlibs(L);
-	luaL_requiref(L, "SimpleDraw", lua_mymodule, 1);
+	luaL_requiref(L, "SimpleDraw", lua_mymodule, 1); //SimpleDraw es el nombre de la biblioteca a importar en lua
 	lua_pop(L, 1);
 	//luaL_dostring(L, "print('Hello from Lua!')");
 	if (luaL_dofile(L, "main.lua"))
@@ -232,8 +265,16 @@ LRESULT CALLBACK WndProc(HWND _hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			{
 				Log(L"D2D1 factory created");
 			}
+			//crear timer para el ciclo de update y draw
+			SetTimer(_hWnd, 1, 16, NULL); //id 1 y 16ms para 60fps aprox
 		}
 		break;
+
+		case WM_TIMER:
+		{
+			InvalidateRect(_hWnd, NULL, FALSE);
+		} break;
+
 		case WM_KEYDOWN:
 		{
 			PWCHAR buffer = new WCHAR[100];
--- x64/Debug/main.lua
+++ x64/Debug/main.lua
@@ -1,11 +1,19 @@
-print("hola mundo desde luAAa")
+local simpledraw = require "SimpleDraw"
+
+print("hola mundo desde main.lua")
 
 function Draw(dt)
-	print("draw desde lua" .. dt)
-end
-
-function functionFromC(dt)
-	print("enviado desde C: " .. dt)
-	testlib.log("la magnitud es " .. testlib.vec2mag(4,3))
-
+	--print("draw desde lua" .. dt)
+	SimpleDraw.Clear(20,20,20)
+	SimpleDraw.SetBrushColor(128,0,0)
+	simpledraw.DrawCircle(100,50,28)
+	SimpleDraw.SetBrushColor(0,128,0)
+	simpledraw.DrawCircle(150,100,28)
+	SimpleDraw.SetBrushColor(0,0,128)
+	simpledraw.DrawCircle(200,150,28)
+	SimpleDraw.SetBrushColor(255,255,0)
+	SimpleDraw.DrawLine(100,100,500,100)
+	SimpleDraw.DrawLine(100,500,500,500)
+	SimpleDraw.SetBrushColor(200,200,255)
+	SimpleDraw.DrawRect(300,300,100,100)
 end
